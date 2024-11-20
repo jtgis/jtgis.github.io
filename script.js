4466ae7137042e8ddd12c87ef53f4725298a1233
jtgis <46830116+jtgis@users.noreply.github.com>
2024-11-19 21:43:00 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showNextSlide() {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length; // Loop back to the first slide
+    slides[currentSlide].classList.add('active');
+}
+
+// Automatically change slides every 5 seconds
+setInterval(showNextSlide, 8000);
+
+// Show the first slide initially
+slides[currentSlide].classList.add('active');
